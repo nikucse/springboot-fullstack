4dac8b4f7fc52733e3fb4edaeeb3920de7208f99
nikucse <nikucse@gmail.com>
2024-09-23 23:53:57 +0530

    added cd

---
+++ backend/src/main/java/com/learn/fullstack/customer/CustomerConstant.java
@@ -0,0 +1,7 @@
+package com.learn.fullstack.customer;
+
+public class CustomerConstant {
+    public  static final  String CUSTOMER_NOT_FOUND_MSG = "Customer with id [%s] not found";
+    public  static final  String CUSTOMER_ALREADY_EXIST_MSG = "email already exist.";
+    public  static final  String NO_DATA_CHANGES_MSG = "no data changes found";
+}
